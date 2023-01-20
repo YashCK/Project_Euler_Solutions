4824545646eb10c692e6bca5324876eb8b6e9cd6
YashCK <yashck2452@gmail.com>
2023-01-19 17:55:41 -0800

    Finished Problem 13

--- First_100_Problems/OneToTen.java
+++ First_100_Problems/OneToTen.java
@@ -21,52 +21,6 @@ import java.util.ArrayList;
 
 public class OneToTen {
 
-    public static void main(String[] args) {
-
-        OneToTen solution = new OneToTen();
-
-        // Problem 1
-        System.out.println("P1: " + solution.sumOfMultiples3o5(1000));
-        // Problem 2
-        System.out.println("P2: " + solution.sumOfEvenFibonacci(4000000));
-        // Problem 3
-        System.out.println("P3: " + solution.findLargestPrimeFactor(600851475143L));
-        // Problem 4
-        System.out.println("P4: " + solution.findLargestPalindrome(3));
-        // Problem 5
-        System.out.println("P5: " + solution.minMaxLCM(1, 20));
-        // Problem 6
-        System.out.println("P6: " + solution.sumSquareDifference(1, 100));
-        // Problem 7
-        System.out.println("P7: " + solution.nthPrime(10001));
-        // Problem 8
-        String stringNum = "73167176531330624919225119674426574742355349194934" + 
-        "96983520312774506326239578318016984801869478851843" + 
-        "85861560789112949495459501737958331952853208805511" +
-        "12540698747158523863050715693290963295227443043557" + 
-        "66896648950445244523161731856403098711121722383113" +
-        "62229893423380308135336276614282806444486645238749" +
-        "30358907296290491560440772390713810515859307960866" +
-        "70172427121883998797908792274921901699720888093776" +
-        "65727333001053367881220235421809751254540594752243" +
-        "52584907711670556013604839586446706324415722155397" +
-        "53697817977846174064955149290862569321978468622482" +
-        "83972241375657056057490261407972968652414535100474" +
-        "82166370484403199890008895243450658541227588666881" +
-        "16427171479924442928230863465674813919123162824586" +
-        "17866458359124566529476545682848912883142607690042" +
-        "24219022671055626321111109370544217506941658960408" +
-        "07198403850962455444362981230987879927244284909188" +
-        "84580156166097919133875499200524063689912560717606" +
-        "05886116467109405077541002256983155200055935729725" + 
-        "71636269561882670428252483600823257530420752963450";
-        System.out.println("P8: " + solution.largestProductInSeries(stringNum, 13));
-        //Problem 9
-        System.out.println("P9: " + solution.findSpecialPythagoreanTriplet(1000))   ;
-        //Problem 10
-        System.out.println("P10: " + solution.sumOfPrimes(2000000));
-    }
-
     //Problem 1
     public int sumOfMultiples3o5(int max) {
         int sum = 0;
@@ -355,6 +309,53 @@ public class OneToTen {
             }
         }
         return true;
+    }
+
+    //Main Method
+    public static void main(String[] args) {
+
+        OneToTen solution = new OneToTen();
+
+        // Problem 1
+        System.out.println("P1: " + solution.sumOfMultiples3o5(1000));
+        // Problem 2
+        System.out.println("P2: " + solution.sumOfEvenFibonacci(4000000));
+        // Problem 3
+        System.out.println("P3: " + solution.findLargestPrimeFactor(600851475143L));
+        // Problem 4
+        System.out.println("P4: " + solution.findLargestPalindrome(3));
+        // Problem 5
+        System.out.println("P5: " + solution.minMaxLCM(1, 20));
+        // Problem 6
+        System.out.println("P6: " + solution.sumSquareDifference(1, 100));
+        // Problem 7
+        System.out.println("P7: " + solution.nthPrime(10001));
+        // Problem 8
+        String stringNum = "73167176531330624919225119674426574742355349194934" + 
+        "96983520312774506326239578318016984801869478851843" + 
+        "85861560789112949495459501737958331952853208805511" +
+        "12540698747158523863050715693290963295227443043557" + 
+        "66896648950445244523161731856403098711121722383113" +
+        "62229893423380308135336276614282806444486645238749" +
+        "30358907296290491560440772390713810515859307960866" +
+        "70172427121883998797908792274921901699720888093776" +
+        "65727333001053367881220235421809751254540594752243" +
+        "52584907711670556013604839586446706324415722155397" +
+        "53697817977846174064955149290862569321978468622482" +
+        "83972241375657056057490261407972968652414535100474" +
+        "82166370484403199890008895243450658541227588666881" +
+        "16427171479924442928230863465674813919123162824586" +
+        "17866458359124566529476545682848912883142607690042" +
+        "24219022671055626321111109370544217506941658960408" +
+        "07198403850962455444362981230987879927244284909188" +
+        "84580156166097919133875499200524063689912560717606" +
+        "05886116467109405077541002256983155200055935729725" + 
+        "71636269561882670428252483600823257530420752963450";
+        System.out.println("P8: " + solution.largestProductInSeries(stringNum, 13));
+        //Problem 9
+        System.out.println("P9: " + solution.findSpecialPythagoreanTriplet(1000))   ;
+        //Problem 10
+        System.out.println("P10: " + solution.sumOfPrimes(2000000));
     }
 
 }
